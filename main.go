7599b2546c1160e78a4bf7e6c817152d5733c499
Andrew Ayer <agwa@andrewayer.name>
2020-02-17 08:46:49 -0500

    Rework the behavior a bit

--- main.go
+++ main.go
@@ -7,19 +7,26 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
+	"net/http"
 )
 
 type DummySigner struct {
-	PubKey  ssh.PublicKey
-	Comment string
-	Used    bool
+	PubKey   ssh.PublicKey
+	Comment  string
+	Tried    bool
+	Accepted bool
 }
 
 func (signer *DummySigner) PublicKey() ssh.PublicKey {
+	signer.Tried = true
 	return signer.PubKey
 }
 func (signer *DummySigner) Sign(rand io.Reader, data []byte) (*ssh.Signature, error) {
-	signer.Used = true
+	signer.Accepted = true
+	if signer.Comment != "" {
+		log.Printf("Server accepted '%s'.", signer.Comment)
+	}
 	return &ssh.Signature{
 		Format: signer.PubKey.Type(),
 	}, nil
@@ -47,19 +54,54 @@ func MakeAuthMethod(dummySigners []*DummySigner) ssh.AuthMethod {
 	return ssh.PublicKeysCallback(func() ([]ssh.Signer, error) { return signers, nil })
 }
 
-func main() {
-	if len(os.Args) != 4 {
-		log.Fatal("Usage: whoarethey SERVER USERNAME KEYSFILE")
-	}
-	server, username, keysFileName := os.Args[1], os.Args[2], os.Args[3]
-
+func LoadSignersFromFile(keysFileName string) ([]*DummySigner, error) {
 	keysFileBytes, err := ioutil.ReadFile(keysFileName)
 	if err != nil {
-		log.Fatal("Failed to read keys file: ", err)
+		return nil, err
 	}
 	signers, err := ParseAuthorizedKeys(keysFileBytes)
 	if err != nil {
-		log.Fatal("Failed to parse keys file: ", err)
+		return nil, err
+	}
+	return signers, nil
+}
+func LoadSignersFromGitHub(username string) ([]*DummySigner, error) {
+	url := "https://github.com/" + username + ".keys"
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("Error retrieving %s: %s", url, resp.Status)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	signers, err := ParseAuthorizedKeys(body)
+	if err != nil {
+		return nil, err
+	}
+	return signers, nil
+}
+func LoadSigners(source string) ([]*DummySigner, error) {
+	if strings.HasPrefix(source, "github:") {
+		return LoadSignersFromGitHub(strings.TrimPrefix(source, "github:"))
+	} else {
+		return LoadSignersFromFile(source)
+	}
+}
+
+func main() {
+	if len(os.Args) != 4 {
+		log.Fatal("Usage: whoarethey SERVER USERNAME KEYSFILE|github:USERNAME")
+	}
+	server, username, keysSource := os.Args[1], os.Args[2], os.Args[3]
+
+	signers, err := LoadSigners(keysSource)
+	if err != nil {
+		log.Fatal("Failed to load public keys: ", err)
 	}
 
 	config := &ssh.ClientConfig{
@@ -69,14 +111,23 @@ func main() {
 	}
 
 	_, err = ssh.Dial("tcp", server, config)
-	success := false
+	numTried := 0
+	numAccepted := 0
 	for _, signer := range signers {
-		if signer.Used {
-			fmt.Println(signer.Comment)
-			success = true
+		if signer.Tried {
+			numTried++
+		}
+		if signer.Accepted {
+			numAccepted++
 		}
 	}
-	if !success {
-		log.Fatal("Failed to dial server: ", err)
+	if numAccepted > 0 {
+		fmt.Printf("Server accepted %d of %d keys.\n", numAccepted, numTried)
+		os.Exit(0)
+	} else if numTried < len(signers) {
+		log.Fatal("Error dialing server: ", err)
+	} else {
+		fmt.Printf("Server accepted no keys.\n")
+		os.Exit(10)
 	}
 }
